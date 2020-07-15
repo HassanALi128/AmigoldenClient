d77b921faddf3714e43bfc0a555a9edd65ca8521
Hassan Ali <hali6338@gmail.com>
2020-07-15 18:40:17 +0500

    request with right paramaters

--- Amigolden/src/app/pages/login/login.page.ts
+++ Amigolden/src/app/pages/login/login.page.ts
@@ -4,19 +4,19 @@ import { AuthenticationService } from 'src/app/services/authentication/authentic
 import { Router } from '@angular/router';
 import { Identity } from 'src/app/services/identity/identity.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit, AfterViewInit {
+export class LoginPage implements OnInit {
   form: FormGroup;
-  @ViewChild('myForm', {static: false}) myForm: ElementRef;
   loginUrl = environment.apiUrl + '/Account/ExternalLogin';
   constructor(
     private authManager: AuthenticationService,
     private identity: Identity, private router: Router,
+    private http: HttpClient
     ) {
       this.form = new FormGroup({
         provider: new FormControl('', {
@@ -29,9 +29,6 @@ export class LoginPage implements OnInit, AfterViewInit {
         }),
       });
      }
-  ngAfterViewInit(): void {
-    
-  }
 
   ngOnInit() {
     let qs = window.location.search;
@@ -52,7 +49,14 @@ export class LoginPage implements OnInit, AfterViewInit {
     }
   }
   submit() {
-    this.myForm.nativeElement.submit();
-    // console.log(this.form.value);
+    const formData = new FormData();
+    formData.append('provider', this.form.value.provider);
+    formData.append('entryCode', this.form.value.entryCode);
+    // debugger;
+    this.http.post<any>(this.loginUrl, formData).subscribe(
+      (res) => console.log(res),
+      (err) => console.log(err)
+    );
+    // this.myForm.nativeElement.submit();
   }
 }
